bd8efa8aa6dbc7ba66fec5f7899d7e99517f702e
Vaibhav Rawat <vaibhrawat@gmail.com>
2022-03-28 12:25:06 +0530

    added name and email form to rawreal page

---
+++ rawreal.js
@@ -0,0 +1,27 @@
+/*function sendEmail() {
+	Email.send({
+    SecureToken : "3dec8523-4503-48fa-8e80-7b74cb03385b",
+    To : 'kataizeheremail@gmail.com',
+    From : "vaibhrawat@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert("mail has been sent sucessfully")
+);
+}*/
+
+
+const btn = document.querySelector('button')
+const inputs = document.querySelector('form')
+btn.addEventListener('click', () => {
+	Email.send({
+		Host: "smtp.mailtrap.io",
+		Username: "b6dc185e7e3d7c",
+		Password: "7c47398d8bd426",
+		To: "kataizeheremail@gamil.com",
+		From: "vaibhrawat@gamil.com",
+		Subject: document.getElementById("name").value,
+		Body: document.getElementById("email").value
+
+	}).then(msg => alert("success"))
+})
